2b57a7ddd3fb4eb9751999478036f919522fec83
bradrust <rust.brad@gmail.com>
2016-02-26 11:12:13 -0700

    * support quartz fromCron

--- dist/angular-cron-jobs.js
+++ dist/angular-cron-jobs.js
@@ -1,6 +1,6 @@
 /**
  * UI Component For Creating Cron Job Syntax To Send To Server
- * @version v1.4.1 - 2016-02-24 * @link https://github.com/jacobscarter/angular-cron-jobs
+ * @version v1.4.1 - 2016-02-26 * @link https://github.com/jacobscarter/angular-cron-jobs
  * @author Jacob Carter <jacob@ieksolutions.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
@@ -260,41 +260,42 @@ angular.module('angular-cron-jobs').factory('cronService', function() {
     };
 
     service.fromCron = function(value) {
+
         //  console.log('set cron fired!');
-       var cron = value.replace(/\s+/g, ' ').split(' ');
-       var frequency = {base: '1'}; // default: every minute
+        var cron = value.replace(/\s+/g, ' ').split(' ');
+        var frequency = {base: '1'}; // default: every minute
 
-       if(cron[0] === '*' && cron[1] === '*' && cron[2] === '*' && cron[3] === '*'  && cron[4] === '*') {
-           frequency.base = 1; // every minute
-       } else if(cron[1] === '*' && cron[2] === '*' && cron[3] === '*'  && cron[4] === '*') {
-           frequency.base = 2; // every hour
-       } else if(cron[2] === '*' && cron[3] === '*'  && cron[4] === '*') {
-           frequency.base = 3; // every day
-       } else if(cron[2] === '*' && cron[3] === '*') {
-           frequency.base = 4; // every week
-       } else if(cron[3] === '*' && cron[4] === '*') {
-           frequency.base = 5; // every month
-       } else if(cron[4] === '*') {
-           frequency.base = 6; // every year
-       }
+        if(cron[0] === '*' && cron[1] === '*' && cron[2] === '*' && cron[3] === '*'  && cron[4] === '*') {
+            frequency.base = 1; // every minute
+        } else if(cron[1] === '*' && (cron[2] === '*' || cron[2] === '?') && cron[3] === '*'  && cron[4] === '*') {
+            frequency.base = 2; // every hour
+        } else if((cron[2] === '*' || cron[2] === '?') && cron[3] === '*'  && cron[4] === '*') {
+            frequency.base = 3; // every day
+        } else if((cron[2] === '*' || cron[2] === '?') && cron[3] === '*') {
+            frequency.base = 4; // every week
+        } else if(cron[3] === '*' && (cron[4] === '*' || cron[4] === '?')) {
+            frequency.base = 5; // every month
+        } else if(cron[4] === '*') {
+            frequency.base = 6; // every year
+        }
 
-       // console.log('frequency should be 5: ', frequency, cron);
+        // console.log('frequency should be 5: ', frequency, cron);
 
-       if (cron[0] !== '*') {
-           frequency.minuteValue = parseInt(cron[0]);
-       }
-       if (cron[1] !== '*') {
-           frequency.hourValue = parseInt(cron[1]);
-       }
-       if (cron[2] !== '*') {
-           frequency.dayOfMonthValue = parseInt(cron[2]);
-       }
-       if (cron[3] !== '*') {
-           frequency.monthValue = parseInt(cron[3]);
-       }
-       if (cron[4] !== '*') {
-           frequency.dayValue = parseInt(cron[4]);
-       }
+        if (cron[0] !== '*') {
+            frequency.minuteValue = parseInt(cron[0]);
+        }
+        if (cron[1] !== '*') {
+            frequency.hourValue = parseInt(cron[1]);
+        }
+        if (cron[2] !== '*' && cron[2] !== '?' ) {
+            frequency.dayOfMonthValue = parseInt(cron[2]);
+        }
+        if (cron[3] !== '*') {
+            frequency.monthValue = parseInt(cron[3]);
+        }
+        if (cron[4] !== '*' && cron[4] !== '?') {
+            frequency.dayValue = parseInt(cron[4]);
+        }
 
        //frequency.base += ''; // 'cast' to string in order to set proper value on "every" modal
 
--- src/cronService.js
+++ src/cronService.js
@@ -36,41 +36,42 @@ angular.module('angular-cron-jobs').factory('cronService', function() {
     };
 
     service.fromCron = function(value) {
+
         //  console.log('set cron fired!');
-       var cron = value.replace(/\s+/g, ' ').split(' ');
-       var frequency = {base: '1'}; // default: every minute
+        var cron = value.replace(/\s+/g, ' ').split(' ');
+        var frequency = {base: '1'}; // default: every minute
 
-       if(cron[0] === '*' && cron[1] === '*' && cron[2] === '*' && cron[3] === '*'  && cron[4] === '*') {
-           frequency.base = 1; // every minute
-       } else if(cron[1] === '*' && cron[2] === '*' && cron[3] === '*'  && cron[4] === '*') {
-           frequency.base = 2; // every hour
-       } else if(cron[2] === '*' && cron[3] === '*'  && cron[4] === '*') {
-           frequency.base = 3; // every day
-       } else if(cron[2] === '*' && cron[3] === '*') {
-           frequency.base = 4; // every week
-       } else if(cron[3] === '*' && cron[4] === '*') {
-           frequency.base = 5; // every month
-       } else if(cron[4] === '*') {
-           frequency.base = 6; // every year
-       }
+        if(cron[0] === '*' && cron[1] === '*' && cron[2] === '*' && cron[3] === '*'  && cron[4] === '*') {
+            frequency.base = 1; // every minute
+        } else if(cron[1] === '*' && (cron[2] === '*' || cron[2] === '?') && cron[3] === '*'  && cron[4] === '*') {
+            frequency.base = 2; // every hour
+        } else if((cron[2] === '*' || cron[2] === '?') && cron[3] === '*'  && cron[4] === '*') {
+            frequency.base = 3; // every day
+        } else if((cron[2] === '*' || cron[2] === '?') && cron[3] === '*') {
+            frequency.base = 4; // every week
+        } else if(cron[3] === '*' && (cron[4] === '*' || cron[4] === '?')) {
+            frequency.base = 5; // every month
+        } else if(cron[4] === '*') {
+            frequency.base = 6; // every year
+        }
 
-       // console.log('frequency should be 5: ', frequency, cron);
+        // console.log('frequency should be 5: ', frequency, cron);
 
-       if (cron[0] !== '*') {
-           frequency.minuteValue = parseInt(cron[0]);
-       }
-       if (cron[1] !== '*') {
-           frequency.hourValue = parseInt(cron[1]);
-       }
-       if (cron[2] !== '*') {
-           frequency.dayOfMonthValue = parseInt(cron[2]);
-       }
-       if (cron[3] !== '*') {
-           frequency.monthValue = parseInt(cron[3]);
-       }
-       if (cron[4] !== '*') {
-           frequency.dayValue = parseInt(cron[4]);
-       }
+        if (cron[0] !== '*') {
+            frequency.minuteValue = parseInt(cron[0]);
+        }
+        if (cron[1] !== '*') {
+            frequency.hourValue = parseInt(cron[1]);
+        }
+        if (cron[2] !== '*' && cron[2] !== '?' ) {
+            frequency.dayOfMonthValue = parseInt(cron[2]);
+        }
+        if (cron[3] !== '*') {
+            frequency.monthValue = parseInt(cron[3]);
+        }
+        if (cron[4] !== '*' && cron[4] !== '?') {
+            frequency.dayValue = parseInt(cron[4]);
+        }
 
        //frequency.base += ''; // 'cast' to string in order to set proper value on "every" modal
 
